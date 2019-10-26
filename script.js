90b8209306189b533a9e46ee7bd07c64ec5c1008
Brodie <brodiecarlisle@gmail.com>
2019-10-26 17:02:10 -0600

    added POST

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const mysql = require('mysql');
 
 const app = express()
 
+app.use(express.json())
+
 const connection = mysql.createConnection({
-  host: 'sql3.freesqldatabase.com',
-  user: 'sql3309579',
-  password: 'MmPNcrPPhy',
-  database: 'sql3309579'
+
 });
 
 connection.connect(function(error){
@@ -22,11 +21,22 @@ app.get('/', function(req,res){
   connection.query("SELECT * FROM ToDo", function(error, rows, fields){
     if(!error){
       console.log('sucessful query');
-      console.log(rows)
+      res.send(rows)
       //query results
     } else{
       console.log('There was an error in the query')
     }
+  })
+})
+
+app.post('/', function(req,res){
+  const body = req.body
+  if(!body){
+    console.log('no data in body')
+  }
+  connection.query("INSERT INTO ToDo SET ? ", body, function(error, results, fields){
+    if(error) console.log('error')
+    res.send(JSON.stringify(results))
   })
 })
 
